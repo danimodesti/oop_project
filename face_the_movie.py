964d6d6a6c4f07a92c2dc024d8e5c263d7ec2033
Dani <dani.modesti@usp.br>
2022-07-12 18:15:30 -0300

    Colocando arquivos iniciais - main e a classe que executa o jogo

---
+++ face_the_movie.py
@@ -0,0 +1,10 @@
+class FaceTheMovie:
+    """
+    Esta é a classe para executar as regras do programa de Face the Movie.
+    """
+    def __init__(self):
+        # Aqui a gente executa o jogo, criando (e importando nesse arquivo no inicio tb)
+        # todos os objetos necessarios etc e fazemos quantas rodadas quisermos (a gente
+        # pode se guiar pela pontuaçao, pelo tempo ou pelos filmes ainda não 'descartados'
+        # do tabuleiro na nossa condicao de continuacao de rodadas)
+        pass
---
+++ main.py
@@ -0,0 +1,18 @@
+#
+# Ana Cristina Silva de Oliveira — 11965630
+# Danielle Modesti — 12543544
+# Laura Ferré Scotelari — 12543436
+# Rebeca Vieira Carvalho — 12543530
+# 
+# POO — 3o semestre — Professor Delamaro
+# 
+# Projeto Final - POO
+
+from face_the_movie import *
+
+def main():
+    # Jogar `Face the Movie` 
+    play_game = FaceTheMovie()
+
+if __name__ == "__main__":
+    main()
