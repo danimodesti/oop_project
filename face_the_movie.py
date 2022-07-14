d43c096d8fcb0a34a32748fe40368afc851dcf2a
laura <laura.scotelari@usp.br>
2022-07-14 19:20:11 -0300

    Testando git

--- face_the_movie.py
+++ face_the_movie.py
@@ -7,4 +7,6 @@ class FaceTheMovie:
         # todos os objetos necessarios etc e fazemos quantas rodadas quisermos (a gente
         # pode se guiar pela pontuaçao, pelo tempo ou pelos filmes ainda não 'descartados'
         # do tabuleiro na nossa condicao de continuacao de rodadas)
+
+        #versao laura
         pass
