a20d7e08fd4d17b304c2f04a6b7792fbbc92c5a0
laura <laura.scotelari@usp.br>
2022-07-14 19:54:27 -0300

    Leitura do .csv e dicas

---
+++ movie.py
@@ -0,0 +1,29 @@
+#
+# Ana Cristina Silva de Oliveira — 11965630
+# Danielle Modesti — 12543544
+# Laura Ferré Scotelari — 12543436
+# Rebeca Vieira Carvalho — 12543530
+# 
+# POO — 3o semestre — Professor Delamaro
+# 
+# Projeto Final - POO
+
+class Movie():
+
+    def __init__(self, id, name, director, actor, supporting, genre, year, imdbScore, imgName):
+        self.id = id
+        self.name = name
+        self.director = director
+        self.actor = actor
+        self.supporting = supporting
+        self.year = year
+        self.genre = genre
+        self.imdbScore = imdbScore
+        self.imgName = imgName
+        
+    def __str__(self):
+        string = "\n\tName: "+ str(self.name) + "\n\tDirector: "+str(self.director) +"\n\t"
+        string += "Actor: " + str(self.actor) + "\n\tSupporting: " + str(self.supporting) +"\n\t"
+        string += "Release year: " + str(self.year) + "\n\tGenre: " + str(self.genre) +"\n\t"
+        string += "IMDB Score: " + str(self.imdbScore) + "\n\tImg name: " + str(self.imgName) +"\n"
+        return  string
---
+++ read_csv.py
@@ -0,0 +1,67 @@
+#
+# Ana Cristina Silva de Oliveira — 11965630
+# Danielle Modesti — 12543544
+# Laura Ferré Scotelari — 12543436
+# Rebeca Vieira Carvalho — 12543530
+# 
+# POO — 3o semestre — Professor Delamaro
+# 
+# Projeto Final - POO
+# Leitura dos atributos de um filme atraves de um arquivo .csv
+import csv
+import pandas as pd
+import glob
+import os
+
+from Movie import *
+
+
+
+def readCSV(movies):
+    #Lendo os arquivos de grupos
+    path = os.getcwd()
+    filenames = glob.glob(os.path.join(path, "*.csv"))
+
+    print(path)
+
+    #percorrendo os arquivos .csv de definicao de grupos
+    for file in filenames:
+        try:
+            # lendo arquivos .csv
+            df = pd.read_csv(file)
+            try:
+                #percorrendo as colunas
+                id = pd.read_csv(file).columns[0]
+                name = pd.read_csv(file).columns[1]
+                director = pd.read_csv(file).columns[2]
+                actor = pd.read_csv(file).columns[3]
+                supporting = pd.read_csv(file).columns[4]
+                genre = pd.read_csv(file).columns[5]
+                year = pd.read_csv(file).columns[6]
+                imdbScore = pd.read_csv(file).columns[7]
+                imgName = pd.read_csv(file).columns[8]
+                newMovie = Movie(id, name, director, actor, supporting, genre, year, imdbScore, imgName)
+
+                movies.append(newMovie)
+
+                for j in range(len(df)):
+                    id = df.iloc[j, 0]
+                    name = df.iloc[j, 1]
+                    director = df.iloc[j, 2]
+                    actor = df.iloc[j, 3]
+                    supporting = df.iloc[j, 4]
+                    genre = df.iloc[j, 5]
+                    year = df.iloc[j, 6]
+                    imdbScore = df.iloc[j, 7]
+                    imgName = df.iloc[j, 8]
+
+                    newMovie = Movie(id, name, director, actor, supporting, genre, year, imdbScore, imgName)
+
+                    movies.append(newMovie)
+
+            except:
+                print("Nao foi possivel ler as informacoes do filme")
+        except:
+            print("Nao foi possivel ler o arquivo")
+
+
