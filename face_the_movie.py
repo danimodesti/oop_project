99f1247682949ea4c7421a2524cbe11b5b03b2f8
Rebeca Carvalho <105522204+rebeca-vc@users.noreply.github.com>
2022-07-17 17:22:09 -0300

    Update face_the_movie.py

--- face_the_movie.py
+++ face_the_movie.py
@@ -15,48 +15,14 @@ import re
 
 # Layout 
 # sg.theme('DarkBlue')
-font = ("Dimitri Swank", 40)
-fontHint = ("Dimitri Swank", 15)
-fontName = ("Dimitri Swank", 10)
-fontButton = ("Dimitri", 20)
+font = ("Arial", 40)
+fontHint = ("Arial", 15)
+fontName = ("Arial", 10)
+fontButton = ("Arial", 20)
 fontUserLayout = ("Helvetica", 15)
 fontName = ("Helvetica", 12)
 movies = []
 
-# Elementos graficos ----------------
-title = [
-    [sg.Text('FACE', text_color=('white'), key='-text-', font=font)],
-    [sg.Text('THE', text_color=('white'),key='-text-', font=font)],
-    [sg.Text('MOVIE', text_color=('white'),key='-text-', font=font)]
-]
-
-buttons = [
-    [sg.Button('Jogar', button_color=('white', sg.theme_background_color()), font=fontButton, border_width = 0)],
-    [sg.Button('Ranks', button_color=('white', sg.theme_background_color()), font=fontButton, border_width = 0)],
-    [sg.Button('Sair', button_color=('white', sg.theme_background_color()), font=fontButton, border_width = 0)]
-]
-
-buttonNextHint = [
-    [sg.Button('Proxima Dica', button_color=('white', sg.theme_background_color()), font=fontButton, border_width = 0)]
-]
-
-Layout = [
-    [sg.VPush()],
-    [sg.Column(title) , sg.Column(buttons)],
-    [sg.VPush()]
-]
-
-UserLayout = [
-    [sg.VPush()],
-    [sg.Text('Insira aqui seu nome', text_color=('white'), key='-text-', font=fontUserLayout)],
-    [sg.InputText()],
-    [sg.Button('Entrar', button_color=('white', sg.theme_background_color()), font=fontButton, border_width = 0)],
-    [sg.VPush()]
-]
-
-RankLayout = [
-    [sg.Text('MELHORES PONTUACOES', text_color=('white'), key='-text-', font=font)]
-]
 # ----------------------------------
 
 class FaceTheMovie:
@@ -64,6 +30,13 @@ class FaceTheMovie:
     allMovies = list()
    
     def __init__(self):
+        UserLayout = [
+            [sg.VPush()],
+            [sg.Text('Insira aqui seu nome', text_color=('white'), key='-text-', font=fontUserLayout)],
+            [sg.InputText()],
+            [sg.Button('Entrar', button_color=('white', sg.theme_background_color()), font=fontButton, border_width = 0)],
+            [sg.VPush()]
+        ]
         # Entrada e criacao do objeto User
         windowUser = sg.Window('BEM VINDO', UserLayout, size=(400,200), element_justification = 'c', use_default_focus=False)
         while True:
@@ -83,6 +56,23 @@ class FaceTheMovie:
                     self.mainMenu()
 
     def mainMenu(self):
+        title = [
+            [sg.Text('FACE', text_color=('white'), key='-text-', font=font)],
+            [sg.Text('THE', text_color=('white'),key='-text-', font=font)],
+            [sg.Text('MOVIE', text_color=('white'),key='-text-', font=font)]
+        ]
+
+        buttons = [
+            [sg.Button('Jogar', button_color=('white', sg.theme_background_color()), font=fontButton, border_width = 0)],
+            [sg.Button('Ranks', button_color=('white', sg.theme_background_color()), font=fontButton, border_width = 0)],
+            [sg.Button('Sair', button_color=('white', sg.theme_background_color()), font=fontButton, border_width = 0)]
+        ]
+        Layout = [
+            [sg.VPush()],
+            [sg.Column(title) , sg.Column(buttons)],
+            [sg.VPush()]
+        ]
+
         levelNumber = self.user.getLevel()
         level = 'Padrao'
         if levelNumber == 0:
@@ -115,25 +105,32 @@ class FaceTheMovie:
                 break
 
     def showRank(self):
+        RankLayout = [
+            [sg.Text('MELHORES PONTUACOES', text_color=('white'), key='-text-', font=font)]
+        ]
         # Ler csv do rank e mostrar na tela
         print("SHoW RaNK")
 
     def newGame(self):
+        
         #preencher lista de filmes com as informações do .csv
         readCSV(self.allMovies)
 
         # Teste - printando os filmes lidos 
-        print("Printando filmes ------------------------------------------")
-        for i in range(len(self.allMovies)):
-            print(self.allMovies[i])
+        # print("Printando filmes ------------------------------------------")
+        # for i in range(len(self.allMovies)):
+        #     print(self.allMovies[i])
 
         self.newPhase()
 
     def newPhase(self):
+        buttonNextHint = [
+            [sg.Button('Proxima Dica', button_color=('white', sg.theme_background_color()), font=fontButton, border_width = 0)]
+        ]
         
         # Cria o gameboard de uma partida
         self.gameboard = gameboard(self.allMovies, 15, self.user.getLevel())
-        self.gameboard.showMovies()
+        # self.gameboard.showMovies()
 
 
         #lista com nomes das imagens e dos filmes que serao as alternativas
@@ -141,7 +138,7 @@ class FaceTheMovie:
         names = []
 
         self.gameboard.loadImagesAndNames(images, names)
-        print(f"IMAGES: {len(images)} AND YUP: {names}")
+        # print(f"IMAGES: {len(images)} AND YUP: {names}")
         
         try:
             #adicionando as imagens na tela
@@ -149,13 +146,13 @@ class FaceTheMovie:
 
             for i in range(self.gameboard.getNumMovies()):
                 #definindo as 'keys' das imagens e dos botoes
-                imgKey = "-img" + str(i) + "-"
-                btnKey = "-btnDesc"+ str(i) + "-"
-                btnGuessKey = "-btnAdv"+ str(i) + "-"
+                imgKey = "-img" + str(images[i][0]) + "-"
+                btnKey = "-btnDesc"+ str(images[i][0]) + "-"
+                btnGuessKey = "-btnAdv"+ str(images[i][0]) + "-"
                 print(f"IMAGES: {images[i]}, {imgKey}, {names[i]}, {btnKey}")
 
                 #chamando a funcao de criacao de layout e adicionando na lista de layouts
-                imgLayouts.append(self.makeImageLayout(images[i], imgKey, names[i], btnKey, btnGuessKey))
+                imgLayouts.append(self.makeImageLayout(images[i][1], imgKey, names[i], btnKey, btnGuessKey))
 
         except:
             print("Não foi possível encontrar as imagens!")
@@ -164,6 +161,8 @@ class FaceTheMovie:
 
         # sorteando um filme para ser adivinhado
         self.gameboard.drawSelectedMovie()
+        print(self.gameboard.selectedMovie.name)
+        print(self.gameboard.getIdSelectedMovie())
         possibleHints = [1, 2, 3, 4, 5, 6]
 
         # selecionando uma dica
@@ -172,7 +171,7 @@ class FaceTheMovie:
         #layout da pagina
         Layout2 = [
             #Elementos da pagina
-            [sg.Text('PREPARAR', key='-text-', font=font)],
+            # [sg.Text('PREPARAR', key='-text-', font=font)],
             [sg.Text(hint, key='-textHint-', font=fontHint)],
         ]
 
@@ -184,6 +183,8 @@ class FaceTheMovie:
             if i != 0 and i % 7 == 0 :
                 Layout2 += [line] 
                 line = []
+
+        Layout2 += [line]
 
         Layout2 += [[sg.Column(buttonNextHint)]]
         
@@ -208,13 +209,19 @@ class FaceTheMovie:
             if eventos.startswith('-btnDesc'):
                 
                 #verificando qual e o numero da alternativa que foi selecionada
-                numImg = re.sub("[^0-9]", "", eventos)
+                idImg = re.sub("[^0-9]", "", eventos)
+
+                index = 0
+                for i in range(len(images)):
+                    if images[i][0] == int(idImg):
+                        index = i
+                        break
 
                 #descolorindo a imagem referente a alternativa selecionada
-                imgGray = self.decolorizeImage(images[int(numImg)])
+                imgGray = self.decolorizeImage(images[index][1])
 
                 #determinando qual sera o nome da imagem
-                imgName = "-img" + numImg + "-"
+                imgName = "-img" + idImg + "-"
 
                 if window[eventos].get_text() == 'Descartar': 
                     #atualizando a imagem do layout para a imagem em preto e branco
@@ -222,9 +229,26 @@ class FaceTheMovie:
                     window[eventos].update('Reverter')
 
                 elif window[eventos].get_text() == 'Reverter': 
-                    print(images[int(numImg)])
-                    window[imgName].update(images[int(numImg)])
+                    print(images[int(idImg)])
+                    window[imgName].update(images[index][1])
                     window[eventos].update('Descartar')
+
+            if eventos.startswith('-btnAdv'):
+                #verificando qual e o numero da alternativa que foi selecionada
+                idMovie = re.sub("[^0-9]", "", eventos)
+                print(f"ID MOVIE: {idMovie}")
+
+                if self.gameboard.getIdSelectedMovie() == int(idMovie):
+                    sg.popup_no_titlebar('ACERTOU', keep_on_top=True, background_color='black')
+                    window.close()
+                    self.endGame()
+                else:
+                    self.gameboard.decreaseScore(150)
+                    sg.popup_no_titlebar('ERROU', keep_on_top=True, background_color='black')
+
+
+    def endGame(self):
+        self.mainMenu()
 
 
     def decolorizeImage(self, imgName):
@@ -249,7 +273,7 @@ class FaceTheMovie:
             [sg.Image(nameImg, key=imgKey)],
             [sg.Text(nameMovie, text_color=('white'), font=fontName)],
             [sg.Button('Descartar', key=btnKey, button_color=('white', sg.theme_background_color()), font=fontHint, border_width = 0)],
-            # [sg.Button('Adivinhar', key=btnGuessKey, button_color=('white', sg.theme_background_color()), font=fontHint, border_width = 0)]
+            [sg.Button('Adivinhar', key=btnGuessKey, button_color=('white', sg.theme_background_color()), font=fontHint, border_width = 0)]
         ]
 
         # ------------------- Window Creation -------------------
